67f59e1a0a53c2831b0cfbcb668c3d965c890225
mariai <irene.maria@assurity.co.nz>
2021-07-13 23:03:01 +1200

    Initial Commit

--- src/test/java/calculator/CalculatorSteps.java
+++ src/test/java/calculator/CalculatorSteps.java
@@ -8,10 +8,6 @@ import static org.junit.Assert.assertEquals;
 public class CalculatorSteps {
     private int result;
 
-
-
-
-
     @Then("the answer is {int}")
     public void theAnswerIs(int arg0) {
         assertEquals(arg0, result);
